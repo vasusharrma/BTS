39ddd86d4b5debc37a242e5c7a1418a4f763f48f
vasusharrma <sharmavashu099@gmail.com>
2025-02-18 19:42:43 +0530

    middlewares

--- hkrts/src/index.ts
+++ hkrts/src/index.ts
@@ -1,44 +1,47 @@
-import express, { Request, Response } from "express";
-import dotenv from "dotenv"
-import bodyPareser from 'body-parser'
-import fs from 'fs'
+import express, { Request, Response } from 'express';
+import bodyParser from "body-parser"
 import path from 'path'
-
+import { readFile } from 'fs'
 
 const app = express();
 
 
-//middleware
-app.use(bodyPareser.json());
-dotenv.config();
+app.use(bodyParser.json());
+
 
 app.get('/', (req: Request, res: Response): void => {
-  const filePath = path.join(__dirname, "sample.txt");
-  fs.readFile(filePath, 'utf-8', (err, data) => {
+  const PATH: string = path.join(__dirname, "/sample.txt");
+  readFile(PATH, 'utf-8', (err, data) => {
+
     if (err) {
-      return res.status(500).json({ error: "Error reading file" });
+      return res.status(500).json({ error: "Got error while looking for the file" })
     }
     res.json({ content: data });
   })
 
-});
+
+})
+
+
+app.get('/student/:name/:cuid', (req: Request, res: Response): void => {
+  const { cuid, name } = req.params;
+  res.send(`<h1>The student is ${name} & his cuid is ${cuid}</h1>`)
+})
 
 app.post('/api/conversation', (req: Request, res: Response): void => {
-  const students = req.body.students;
+  const students: object = req.body;
   console.log(students);
-  res.json({
-    response: "I got your message"
-  })
+  res.json({ "message": "Data reached succesfully" })
+})
+
+const PORT: number = 9000;
+
+app.listen(PORT, () => {
+  console.log(`server is linstining on port ${PORT}`);
 })
 
 
 
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, (): void => {
-  console.log(`server is listening on port ${PORT}`)
-})
 
 
 
