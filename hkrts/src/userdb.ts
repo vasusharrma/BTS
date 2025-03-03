a1efc2eec23a331c0f9a33c8f0cfd945093c9b53
vasusharrma <sharmavashu099@gmail.com>
2025-03-03 21:46:29 +0530

    info with sign up stored in db

--- hkrts/src/userdb.ts
+++ hkrts/src/userdb.ts
@@ -1,33 +1,63 @@
-import mongoose from "mongoose";
 import express, { Request, Response } from 'express'
+import mongoose from 'mongoose';
+import z from 'zod';
 
 const app = express();
-const mongoURL = "mongodb+srv://Ram:7spze2dy9n@vasusharrmaa.4itmd.mongodb.net/"
-const PORT = 3900;
+const PORT = 8000;
+const dbURL = "mongodb+srv://Ram:7spze2dy9n@vasusharrmaa.4itmd.mongodb.net/userdbapp";
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  email: String
+});
+
+const Users = mongoose.model("Users", userSchema);
+
 app.use(express.json());
 
-
-mongoose.connect(mongoURL)
-  .then(() => console.log("db connected successfully"))
+mongoose.connect(dbURL)
+  .then(() => console.log("db connected succesfully"))
   .catch((err) => console.error("error: ", err));
 
-app.get('/', function (req: Request, res: Response): void {
-  res.send("Homepage");
+
+app.post("/signup", async (req: Request, res: Response): Promise<void> => {
+
+  const username = req.headers.username as string;
+  const password = req.headers.password as string;
+  const email = req.headers.email as string;
+
+  const userExist = await Users.findOne({ username });
+
+  if (userExist) {
+    res.status(411).json({ msg: "Usre already exits" });
+    return;
+  }
+
+  const newUser = new Users({ username, password, email });
+  newUser.save()
+    .then(() => {
+      console.log(`The user with username ${username} succesfully added to the db`)
+      res.status(201).json({ msg: "user created succesfully" })
+    })
+    .catch((err) => console.error("err : ", err));
+
 })
+
+
+
 
 
 app.use((err: unknown, req: Request, res: Response): void => {
-  console.error("err: ", err);
+  console.error("error: ", err);
   if (err instanceof Error) {
-    res.status(500).json({ error: err });
-    return;
+    res.status(401).json({ error: err });
   }
   else {
-    res.status(411).json({ msg: "something up with our server" });
+    res.status(403).json({ msg: "something up with our server" });
   }
 })
 
-
-app.listen(PORT, () => {
-  console.log(`server is live on PORT ${PORT}`);
+app.listen(PORT, function() {
+  console.log(`server is running on port ${PORT}`);
 })
